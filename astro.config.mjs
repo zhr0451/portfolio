f4c3d2de150ed1498b051cf71d5d43bf0fe45409
zhr0451 <1samokhvalov.zs@gmail.com>
2025-09-18 17:38:39 +0300

    added pnpm support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zhr0451.github.io',
-  base: '/portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
